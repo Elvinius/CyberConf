4771a8877815260690a69bc69d7a7e121af13724
Elvin Abbasli <elwinabbasli@gmail.com>
2020-04-28 15:52:27 +0300

    Add the email field

--- js/tickets.js
+++ js/tickets.js
@@ -18,18 +18,4 @@ $("#payment-button").click(function(e) {
     });
 });
 
-$("#signin-button").click(function(e) {
-
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-        form.addEventListener('submit', function(event) {
-            if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-        }, false);
-    });
-});
 
